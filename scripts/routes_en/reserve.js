5ead2b016f6810678a7a0559ea4dc01831b1e1c3
sungwook-park <zeze800525@gmail.com>
2025-07-10 19:27:09 +0900

    Add files via upload

--- scripts/routes_en/reserve.js
+++ scripts/routes_en/reserve.js
@@ -1,17 +1,9 @@
-import '../tasks.js';
-
 export function render(container) {
   container.innerHTML = `
     <div class="box">
       <button onclick="routeTo('home')">🏠 홈으로</button>
-      <h2>예약 확인</h2>
-      <div class="section box">
-        <div id="taskList"></div>
-      </div>
+      <h2>예약</h2>
+      <p>예약 기능 준비 중...</p>
     </div>
   `;
-
-  setTimeout(() => {
-    window.loadTasks('reserve');
-  }, 100);
 }
