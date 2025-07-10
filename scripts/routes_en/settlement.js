52f5b2f7fe83e9ae2837b5d35b27db313c26330c
sungwook-park <zeze800525@gmail.com>
2025-07-10 21:41:30 +0900

    Delete scripts directory

--- scripts/routes_en/settlement.js
+++
@@ -1,9 +0,0 @@
-export function render(container) {
-  container.innerHTML = `
-    <div class="box">
-      <button onclick="routeTo('home')">🏠 홈으로</button>
-      <h2>정산</h2>
-      <p>정산 기능 준비 중...</p>
-    </div>
-  `;
-}
